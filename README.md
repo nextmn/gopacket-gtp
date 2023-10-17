cbd3ef1c05695377930900ed11038764f8da5f90
Louis Royer <infos.louis.royer@gmail.com>
2023-10-17 17:28:37 +0200

    Fix

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # gopacket-gtp
 `gopacket` (v1.1.19) is not serializing GTP Extension Headers in the right way.
-Extension Headers are appended to the buffer instead of prepended.
+Extension Headers are appended to the buffer in multiple times instead of allocating the right size directly.
 This results in a malformed packet since the extension ends up after the payload.
 
 This fork indends to fix this.
--- main.go
+++ main.go
@@ -111,10 +111,22 @@ func (g *GTPv1U) DecodeFromBytes(data []byte, df gopacket.DecodeFeedback) error 
 // SerializationBuffer, implementing gopacket.SerializableLayer.
 // See the docs for gopacket.SerializableLayer for more info.
 func (g *GTPv1U) SerializeTo(b gopacket.SerializeBuffer, opts gopacket.SerializeOptions) error {
-	data, err := b.PrependBytes(gtpMinimumSizeInBytes)
+	extHeadersLen := 0
+	if len(g.GTPExtensionHeaders) > 0 {
+		g.ExtensionHeaderFlag = true
+	}
+	if g.ExtensionHeaderFlag || g.SequenceNumberFlag || g.NPDUFlag {
+		extHeadersLen += 4
+		for _, eh := range g.GTPExtensionHeaders {
+			extHeadersLen += len(eh.Content) + 2 // 2 bytes for Type + Lenght
+
+		}
+	}
+	data, err := b.PrependBytes(gtpMinimumSizeInBytes + extHeadersLen)
 	if err != nil {
 		return err
 	}
+	data[0] = 0
 	data[0] |= (g.Version << 5)
 	data[0] |= (1 << 4)
 	if len(g.GTPExtensionHeaders) > 0 {
@@ -130,26 +142,26 @@ func (g *GTPv1U) SerializeTo(b gopacket.SerializeBuffer, opts gopacket.Serialize
 	data[1] = g.MessageType
 	binary.BigEndian.PutUint16(data[2:4], g.MessageLength)
 	binary.BigEndian.PutUint32(data[4:8], g.TEID)
-	if g.ExtensionHeaderFlag || g.SequenceNumberFlag || g.NPDUFlag {
-		data, err := b.PrependBytes(4)
-		if err != nil {
-			return err
-		}
-		binary.BigEndian.PutUint16(data[:2], g.SequenceNumber)
-		data[2] = g.NPDU
+	if g.SequenceNumberFlag {
+		binary.BigEndian.PutUint16(data[9:10], g.SequenceNumber)
+	}
+	if g.NPDUFlag {
+		data[11] = g.NPDU
+	}
+
+	if g.ExtensionHeaderFlag {
+		index := 12
 		for _, eh := range g.GTPExtensionHeaders {
-			data[len(data)-1] = eh.Type
+			data[index] = eh.Type
 			lContent := len(eh.Content)
 			// extensionLength is in 4-octet units
 			extensionLength := (lContent + 2) / 4
-			// Get two extra byte for the next extension header type and length
-			data, err = b.PrependBytes(lContent + 2)
-			if err != nil {
-				return err
-			}
-			data[0] = byte(extensionLength)
-			copy(data[1:lContent+1], eh.Content)
+			data[index+1] = byte(extensionLength)
+			copy(data[index+2:index+2+lContent], eh.Content)
+			index += 2 + lContent // type + len + content
 		}
+		// last type is 0x00 (No nore extension headers)
+		data[index] = 0x00
 	}
 	return nil
 
